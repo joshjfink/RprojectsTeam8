f7eaa9ec54038a6ecb26c95dd5f3e083faf16849
joshua-james-f <joshua.james.f@gmail.com>
2014-10-04 01:11:40 -0400

    Added LaQuinta parsing syntax for Address, Phone, Fax, latitude, and longitude. This is the basic version acted on a single page.

---
+++ HW2/LQ_ParseList.R
@@ -0,0 +1,29 @@
+check_packages = function(names)
+{
+    for(name in names)
+    {
+        if (!(name %in% installed.packages()))
+            install.packages(name, repos="http://cran.us.r-project.org")
+    
+        library(name, character.only=TRUE)
+    }
+}
+
+library("httr"); library(stringr)
+d = data.frame(name= "La Quinta Inn & Suites Flagstaff",
+               url = "http://www.lq.com/en/findandbook/hotel-details.flagstaff.address.html",
+               stringsAsFactors = FALSE)
+
+attach(d)
+page = GET(url)
+s = content(page, as="text")
+write(s, file="lq/flag.html") 
+
+# Address, Phone and Fax
+contact_info = str_match_all(s, "<p>+([0-9][0-9a-zA-Z ]*), <br/>[ \r\n]*([A-Za-z0-9, ]*)[ \r\n<br/>]*Phone: ([ 0-9-]*)[ \r\n<br/>]*Fax: ([ 0-9-]*)")[[1]][,2:5]
+add_num <- contact_info[1]
+add_csz <- contact_info[2]
+phone <- contact_info[3]
+fax <- contact_info[4]
+# latitude, and longitude
+lat_lon = str_match_all(s, "green.gif|[-]*[0-9]*[.][0-9]*,[-]*[0-9]*[.][0-9]*")[[1]][2,] 
