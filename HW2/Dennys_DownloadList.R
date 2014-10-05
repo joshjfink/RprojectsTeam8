8b7de9598e359964322b597f81a904363659f737
Zheng Ding <jtappler@gmail.com>
2014-10-05 02:19:00 -0400

    Denny: for loop state

--- HW2/Dennys_DownloadList.R
+++ HW2/Dennys_DownloadList.R
@@ -9,8 +9,12 @@ AllStates = c(state.abb) #All 50 states
 urlHead = "http://hosted.where2getit.com/dennys/ajax?&xml_request=%3Crequest%3E%3Cappkey%3E8D6F0428-F3A9-11DD-8BF2-659237ABAA09%3C%2Fappkey%3E%3Cformdata+id%3D%22locatorsearch%22%3E%3Cdataview%3Estore_default%3C%2Fdataview%3E%3Climit%3E3000%3C%2Flimit%3E%3Cgeolocs%3E%3Cgeoloc%3E%3Caddressline%3E"
 urlTail = "%3C%2Faddressline%3E%3Clongitude%3E%3C%2Flongitude%3E%3Clatitude%3E%3C%2Flatitude%3E%3C%2Fgeoloc%3E%3C%2Fgeolocs%3E%3Csearchradius%3E3000|10000%3C%2Fsearchradius%3E%3C%2Fformdata%3E%3C%2Frequest%3E"
 
-dc = content(GET(dc_url), as="text")
-slc= content(GET(slc_url), as="text")
+for (n in 1:50){
+  state = AllStates[n]
+  url = paste(urlHead, state, urlTail, sep = "")
+  st = GET(url)
+  fileName = paste("st",STATE,".xml",sep = "")
+}
 
 write(dc, file="dennys/dc.html")
 write(slc, file="dennys/slc.html")
