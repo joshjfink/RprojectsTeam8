5776debf63f5ea59248152c3ef3cc9ef857acbb8
YanyuLiu <yl304@stat.duke.edu>
2014-10-05 17:01:34 -0400

    Find the reason why the # of names and links are different, 17 hotels share the same link

--- HW2/LQ_list_parse.R
+++ HW2/LQ_list_parse.R
@@ -1,22 +1,19 @@
 # Do parsing stuff
 
 
-# df of Hotel names and urls
-d = data.frame(name= "La Quinta Inn & Suites Flagstaff",
-               url = "http://www.lq.com/en/findandbook/hotel-details.flagstaff.address.html",
-               stringsAsFactors = FALSE) 
-
-
 # get hotel name and link
-
 Name = str_match_all(s, "html\">([A-Za-z0-9'\\() /&\\.-]*)</a><br>\r\n")
 link = str_match_all(s, "<a href=\"([a-z0-9 \\/\\.-]*)\">La Quinta")
+
+#change the data structure to character matrix
 Name = unlist(Name)
 Name = Name[(length(Name)/2+1):length(Name)]
 link = unlist(link)
 link = link[(length(link)/2+1):length(link)]
 link = paste0("http://www.lq.com",link)
-#the number of link and name are different, and hard to match, so what do you think if we can gather the name in the next step?
+
+#The numbers of link and name are different, because 17 hotels own the same link: "/en/findandbook/hotel-details.null.address.html" 
+#so we don't want to match hotel names with links, the only information we need now are the links, but not the names
 link = unique(link)
 Name = unique(Name)
 
