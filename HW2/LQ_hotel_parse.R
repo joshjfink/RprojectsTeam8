af668e4c743db6106aa8112ee63ff485b39831ff
YanyuLiu <yl304@stat.duke.edu>
2014-10-05 20:32:56 -0400

    combine the hotel data

--- HW2/LQ_hotel_parse.R
+++ HW2/LQ_hotel_parse.R
@@ -3,6 +3,7 @@ check_packages(c("httr", "stringr"))
 
 # Create list of files
 f <- list.files(path="lq/hotels", pattern="*.html", full.names=T, recursive=FALSE)
+
 #Contact Info (Address, phone, fax)
 contact <- sapply(f, function(x){
 	a <- readLines(x)
@@ -16,11 +17,23 @@ for(i in 1:length(contact)){
 	phone[i] <- ifelse(class(contact[[i]]) =="character", "NA", contact[[i]][,4])
 	fax[i] <- ifelse(class(contact[[i]]) =="character", "NA", contact[[i]][,5])
 }
+
 # Lattitude and Longitude
 lat_lon <- sapply(f, function(x){
 	a <- readLines(x)
 	b <- paste(a, collapse = "")
-	str_match_all(b, "green.gif|[-]*[0-9]*[.][0-9]*,[-]*[0-9]*[.][0-9]*")
+	str_match_all(b, "green.gif|([-]*[0-9]*[.][0-9]*),([-]*[0-9]*[.][0-9]*)")
 })
-lat_lon <- unique(unlist(lat_lon))
+
+lat_lon_mat <- matrix(NA,length(contact),2)
+for (i in 1:length(contact))
+{lat_lon_mat[i,] <- tail(lat_lon[i][[1]], n=1)[,-1]
+}
+
+#combine hotel data
+hotel_data <- cbind(add1,add2,phone,fax,lat_lon_mat[,1],lat_lon_mat[,2])
+colnames(hotel_data) <- c("address1","address2","phone","fax","latitude","longitude")
+
+# Save results as Rdata file
+save(hotel_data, file="lq/hotel data.Rdata")
 
