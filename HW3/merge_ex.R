af9a4a55278e94c6c10bc191a0b744c44a8f4849
Zheng Ding <jtappler@gmail.com>
2014-10-19 22:48:20 -0400

    geo match

--- HW3/merge_ex.R
+++ HW3/merge_ex.R
@@ -8,11 +8,17 @@ base = '/home/vis/cr173/Sta523/data/parking'
 park = tbl_df(read.csv(paste0(base,"/NYParkingViolations_small.csv"), stringsAsFactors=FALSE))
 
 addr = filter(park, Violation.Precinct <= 34) %>%
-       mutate(House.Number = str_trim(House.Number), Street.Name = str_trim(Street.Name)) %>%
+       mutate(House.Number = str_trim(House.Number)) %>%
        filter(House.Number != "" & Street.Name != "") %>%
        filter(str_detect(House.Number,"[0-9]+")) %>%
+       mutate(Street.Name = toupper(Street.Name)) %>%
+       mutate(Street.Name = str_replace(Street.Name,"TH "," ")) %>%
+       mutate(Street.Name = str_replace(Street.Name, "([0-9]+)ST","\\1")) %>%
+       mutate(Street.Name = str_replace(Street.Name, "([0-9]+)RD","\\1")) %>%
+       mutate(Street.Name = str_replace(Street.Name, "([0-9]+)ND","\\1")) %>%
+       mutate(Street.Name = str_trim(Street.Name)) %>%
        transmute(Violation.Precinct = Violation.Precinct, addr = paste(House.Number, Street.Name)) %>%
-       mutate(addr = tolower(addr))
+       mutate(addr = toupper(addr))
 
 
 pl = readOGR(paste0(base,"/pluto/Manhattan/"),"MNMapPLUTO")
