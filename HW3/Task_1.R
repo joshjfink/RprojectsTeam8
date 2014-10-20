c8159d4b729d5252f79e80a9d3657522ed97e650
joshua-james-f <joshua.james.f@gmail.com>
2014-10-19 21:13:02 -0400

    Added previously missing "stn" street name and intersecting street name seperator; minor bug fixesø

--- HW3/Task_1.R
+++ HW3/Task_1.R
@@ -4,9 +4,14 @@ check_packages(c("dplyr", "data.table", "tidyr", "stringr", "rgdal", "rgeos", "h
 
 dm = tbl_df(read.csv("/home/vis/cr173/Sta523/data/parking/NYParkingViolations.csv", stringsAsFactors=FALSE))
 
-hund <- seq(1, 9000000, 100)
+hund <- seq(1, 9000000, 250)
 hund
 sdm <- dm[hund,] 
+
+stn <- sdm %>%
+  filter(str_detect(Street.Name,"@")) %>%
+  mutate(Street.Name = paste(Street.Name, " ")) %>%
+  separate(Street.Name, c("Street.Name", "a2"), sep = "@") 
 
 sdm2 <- select(sdm, Street.Code1, Street.Code2, Street.Code3, Street.Name, Intersecting.Street, House.Number, Violation.Precinct) %>%
 mutate(Intersecting.Street=ifelse(str_detect(Street.Name,"@"), paste(stn$a2, Intersecting.Street), Intersecting.Street)) %>%
