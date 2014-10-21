b431a4415f1e97b82f88ce2eba11cf9b6051f917
joshua-james-f <joshua.james.f@gmail.com>
2014-10-21 16:44:39 -0400

    Debugged/updated Task_1, deleted exrta "merge" example

--- HW3/Task_1.R
+++ HW3/Task_1.R
@@ -1,32 +1,26 @@
-setwd("/Users/Josh/Desktop/Universe/Computation/GitHub/HW3")
-source("check_packages.R")
-check_packages(c("dplyr", "data.table", "tidyr", "stringr", "rgdal", "rgeos", "httr", "rjson", "RDSTK"))
 
-#only use 1/10 data to run to save time
-dm = read.csv("/home/vis/cr173/Sta523/data/parking/NYParkingViolations_small.csv",
-              stringsAsFactors=FALSE) %>% 
-  as.data.frame() %>%
-  tbl_df()
+source("check_packages.R")
+check_packages(c("dplyr", "data.table", "tidyr", "stringr", "rgdal", "rgeos", "httr", "rjson", "RDSTK", "date", "lubridate"))
+
+#subset data
+dat.master = tbl_df(read.csv("/home/vis/cr173/Sta523/data/parking/NYParkingViolations_small.csv",stringsAsFactors=FALSE))
 
 #choose right issue date
-dm$Issue.Date = mdy(dm$Issue.Date)
-sdm = filter(dm, Issue.Date > "2013/09/01", Issue.Date < "2014/6/30")
+dat.master$Issue.Date = mdy(dat.master$Issue.Date)
+dat.subset = filter(dat.master, Issue.Date > "2013/09/01", Issue.Date < "2014/6/30")
 
 #choose the correct precincts
-sdm = filter(sdm, Violation.Precinct <= 34)
+dat.subset = filter(dat.master, Violation.Precinct <= 34)
 
 #@ should seperate intersecting and street name
-stn <- sdm %>%
+sep_streets <- dat.subset %>%
   filter(str_detect(Street.Name,"@")) %>%
   mutate(Street.Name = paste(Street.Name, " ")) %>%
   separate(Street.Name, c("Street.Name", "a2"), sep = "@") 
 
-sdm2 <- select(sdm, Street.Code1, Street.Code2, Street.Code3, Street.Name, Intersecting.Street, House.Number, Violation.Precinct) %>%
-mutate(Intersecting.Street=ifelse(str_detect(Street.Name,"@"), paste(stn$a2, Intersecting.Street), Intersecting.Street)) %>%
-mutate(Street.Name=ifelse(str_detect(Street.Name,"@"), stn$Street.Name, Street.Name))
+dat.subset <- select(dat.subset, Street.Code1, Street.Code2, Street.Code3, Street.Name, Intersecting.Street, House.Number, Violation.Precinct) %>%
+mutate(Intersecting.Street=ifelse(str_detect(Street.Name,"@"), paste(sep_streets$a2, Intersecting.Street), Intersecting.Street)) %>%
+mutate(Street.Name=ifelse(str_detect(Street.Name,"@"), sep_streets$Street.Name, Street.Name))
 
-addrs = sdm2  %>%
-       mutate(House.Number = str_trim(House.Number), Street.Name = str_trim(Street.Name), Intersecting.Street = str_trim(Intersecting.Street)) %>%
-       filter(str_detect(House.Number,"[0-9]+")) %>%
-       transmute(Violation.Precinct = Violation.Precinct, Intersecting.Street = Intersecting.Street,addr = paste(House.Number, Street.Name, ", NYC,", "NY"))
+
 
