c7fb4fba2d440bd7344fd8ae48692e26060d0f0d
joshua-james-f <joshua.james.f@gmail.com>
2014-10-21 22:28:56 -0400

    Major edits to Task 1 and Task 1 Merged: correct precincts, new subsetting, debugging, additional data cleaning, dplyr bug fixed.

--- HW3/Task_1.R
+++ HW3/Task_1.R
@@ -3,25 +3,26 @@ source("check_packages.R")
 
 check_packages(c("dplyr", "data.table", "tidyr", "stringr", "rgdal", "rgeos", "httr", "rjson", "RDSTK", "date", "lubridate"))
 
-#subset data
-dat.master = tbl_df(read.csv("/home/vis/cr173/Sta523/data/parking/NYParkingViolations_small.csv",stringsAsFactors=FALSE))
+# Read in data
+dat.master = tbl_df(read.csv("/home/vis/cr173/Sta523/data/parking/NYParkingViolations.csv",stringsAsFactors=FALSE))
 
-#choose right issue date
+#choose right issue date & correct precincts
 dat.master$Issue.Date = mdy(dat.master$Issue.Date)
-dat.subset = filter(dat.master, Issue.Date > "2013/09/01", Issue.Date < "2014/6/30")
+dat.subset = filter(dat.master, Issue.Date > "2013/09/01" & Issue.Date < "2014/6/30" & (Violation.Precinct == 5 | Violation.Precinct == 6 | Violation.Precinct == 7 | Violation.Precinct == 9 | Violation.Precinct == 10 | Violation.Precinct == 13 | Violation.Precinct == 14 | Violation.Precinct == 17 | Violation.Precinct == 18 | Violation.Precinct == 19 | Violation.Precinct == 20 | Violation.Precinct == 22 | Violation.Precinct == 23 | Violation.Precinct == 24 | Violation.Precinct == 25 | Violation.Precinct == 26 | Violation.Precinct == 28 | Violation.Precinct == 30 | Violation.Precinct == 32 | Violation.Precinct == 33 | Violation.Precinct == 34))
 
-#choose the correct precincts
-dat.subset = filter(dat.master, Violation.Precinct <= 34)
+# Select and clean addresses
+addr <- select(dat.subset, Street.Code1, Street.Code2, Street.Code3, Street.Name, Intersecting.Street, House.Number, Violation.Precinct) %>% mutate(House.Number = str_trim(House.Number)) %>%
+       filter(House.Number != "" & Street.Name != "") %>%
+       filter(str_detect(House.Number,"[0-9]+")) %>%
+       mutate(Street.Name = toupper(Street.Name)) %>%
+       mutate(Street.Name = str_replace(Street.Name,"TH "," ")) %>%
+       mutate(Street.Name = str_replace(Street.Name, "([0-9]+)ST","\\1")) %>%
+       mutate(Street.Name = str_replace(Street.Name, "([0-9]+)RD","\\1")) %>%
+       mutate(Street.Name = str_replace(Street.Name, "([0-9]+)ND","\\1")) %>%
+       mutate(Street.Name = str_trim(Street.Name)) %>%
+       transmute(Violation.Precinct = Violation.Precinct, addr = paste(House.Number, Street.Name)) %>%
+       mutate(addr = toupper(addr))
 
-#@ should seperate intersecting and street name
-sep_streets <- dat.subset %>%
-  filter(str_detect(Street.Name,"@")) %>%
-  mutate(Street.Name = paste(Street.Name, " ")) %>%
-  separate(Street.Name, c("Street.Name", "a2"), sep = "@") 
-
-dat.subset <- select(dat.subset, Street.Code1, Street.Code2, Street.Code3, Street.Name, Intersecting.Street, House.Number, Violation.Precinct) %>%
-mutate(Intersecting.Street=ifelse(str_detect(Street.Name,"@"), paste(sep_streets$a2, Intersecting.Street), Intersecting.Street)) %>%
-mutate(Street.Name=ifelse(str_detect(Street.Name,"@"), sep_streets$Street.Name, Street.Name))
 
 
-
+  
--- HW3/Task_1_Merged.R
+++ HW3/Task_1_Merged.R
@@ -1,19 +1,19 @@
+# Setup
 source("check_packages.R")
-check_packages(c("dplyr", "date", "data.table", "tidyr", "stringr", "rgdal", "rgeos", "httr", "rjson", "RDSTK"))
+check_packages(c("devtools", "data.table", "tidyr", "stringr", "rgdal", "rgeos", "httr", "rjson", "RDSTK", "date", "lubridate", "Rcpp"))
 
-base = '/home/vis/cr173/Sta523/data/parking'
+# Install updated devtools from GitHub repo to prevent crash
+devtools::install_github("hadley/dplyr")
 
-#still has some merge problem between different R file, so firstly, we will read file separately.
-park = tbl_df(read.csv(paste0(base,"/NYParkingViolations_small.csv"), stringsAsFactors=FALSE))
-#choose right issue date
-park$Issue.Date = mdy(park$Issue.Date)
-park = filter(park, Issue.Date > "2013/09/01", Issue.Date < "2014/6/30")
+# Read in data
+dat.master = tbl_df(read.csv("/home/vis/cr173/Sta523/data/parking/NYParkingViolations.csv",stringsAsFactors=FALSE))
 
-####
-#Conflicts codes deleted...
+#choose right issue date & correct precincts
+dat.master$Issue.Date = mdy(dat.master$Issue.Date)
+dat.subset = filter(dat.master, Issue.Date > "2013/09/01" & Issue.Date < "2014/6/30" & (Violation.Precinct == 5 | Violation.Precinct == 6 | Violation.Precinct == 7 | Violation.Precinct == 9 | Violation.Precinct == 10 | Violation.Precinct == 13 | Violation.Precinct == 14 | Violation.Precinct == 17 | Violation.Precinct == 18 | Violation.Precinct == 19 | Violation.Precinct == 20 | Violation.Precinct == 22 | Violation.Precinct == 23 | Violation.Precinct == 24 | Violation.Precinct == 25 | Violation.Precinct == 26 | Violation.Precinct == 28 | Violation.Precinct == 30 | Violation.Precinct == 32 | Violation.Precinct == 33 | Violation.Precinct == 34))
 
-addr = filter(park, Violation.Precinct <= 34) %>%
-       mutate(House.Number = str_trim(House.Number)) %>%
+# Select and clean addresses
+addr <- select(dat.subset, Street.Code1, Street.Code2, Street.Code3, Street.Name, Intersecting.Street, House.Number, Violation.Precinct) %>% mutate(House.Number = str_trim(House.Number)) %>%
        filter(House.Number != "" & Street.Name != "") %>%
        filter(str_detect(House.Number,"[0-9]+")) %>%
        mutate(Street.Name = toupper(Street.Name)) %>%
@@ -26,6 +26,7 @@ addr = filter(park, Violation.Precinct <= 34) %>%
        mutate(addr = toupper(addr))
 
 
+base = '/home/vis/cr173/Sta523/data/parking'
 pl = readOGR(paste0(base,"/pluto/Manhattan/"),"MNMapPLUTO")
 
 pt = gCentroid(pl,byid=TRUE)
