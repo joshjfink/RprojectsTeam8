ee10e906eb5260e1b5b866f9ad6250e354c006e3
joshua-james-f <joshua.james.f@gmail.com>
2014-11-09 14:41:23 -0500

    Fixed is_undirected fcn

--- hw4/is_undirected.R
+++ hw4/is_undirected.R
@@ -2,22 +2,6 @@
 # Setup
 ####
 
-###### Graph for Testing
-	# Test Graph
-	graph2 = list(A = list(edges   = c(2L),
-	                       weights = c(14)),
-	              B = list(edges   = c(3L,4L),
-	                       weights = c(23,13)),
-	              D = list(edges   = c(1L),
-	                       weights = c(5) ),
-	              F = list(edges   = c(1L,5L),
-	                       weights = c(43,33)),
-	              N = list(edges   = c(1L,2L,4L),
-	                       weights = c(33,22,11)))
-
-	g <- graph2 
-
-
 # Function - is_undirected
 
 # Input - g, a graph object.
@@ -25,16 +9,11 @@
 
 # Description - Check if the graph object is undirected, this is true if all directed edges have a complementary directed edge with the same weight in the opposite direction.
 
-is_undirected = function(g)
-{
-    TRUE
-}
-
-store <- rep(0, length(g))
-for(i in 1:length(g)){
-	store[i] <- sum(g[[i]]$weights)-
-sum(unlist(
-lapply(g, function(x){
-	x$weights[x$edges==i]
-})))
-}
+# Generate matrix
+	n = length(g)
+	m = matrix(0, ncol=n, nrow=n)
+	for(i in 1:n)
+		m[i, g[[i]]$edges] = ifelse(FALSE, g[[i]]$weights/sum(g[[i]]$weights), g[[i]]$weights)
+# Check if directed edges have a complementary directed edge with the same weight in the opposite direction.
+	m.direct <- which(abs(m-t(m))!=0,arr.ind = T)
+	expect_equal(length(m.direct))
