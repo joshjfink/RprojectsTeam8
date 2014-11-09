65a9ac249850ac0e7969c5ef022b3b1f82a93d55
joshua-james-f <joshua.james.f@gmail.com>
2014-11-09 16:30:28 -0500

    Reformatted and debugged is_isomorphic, is_undirected, and is_valid fcns--waiting on is_connected fcn to create predicates.R

--- hw4/is_undirected.R
+++ hw4/is_undirected.R
@@ -1,19 +1,16 @@
-#### 
-# Setup
-####
-
-# Function - is_undirected
-
 # Input - g, a graph object.
 # Output - true if undirected, false if not.
+# Description - Check if directed edges have a complementary directed edge with the same weight in the opposite direction.
 
-# Description - Check if the graph object is undirected, this is true if all directed edges have a complementary directed edge with the same weight in the opposite direction.
-
-# Generate matrix
-	n = length(g)
-	m = matrix(0, ncol=n, nrow=n)
-	for(i in 1:n)
-		m[i, g[[i]]$edges] = ifelse(FALSE, g[[i]]$weights/sum(g[[i]]$weights), g[[i]]$weights)
-# Check if directed edges have a complementary directed edge with the same weight in the opposite direction.
-	m.direct <- which(abs(m-t(m))!=0,arr.ind = T)
-	expect_equal(length(m.direct))
+is_undirected = function(g)
+{
+    # Generate matrix
+		n = length(g)
+		m = matrix(0, ncol=n, nrow=n)
+		for(i in 1:n){
+			m[i, g[[i]]$edges] = ifelse(FALSE, g[[i]]$weights/sum(g[[i]]$weights), g[[i]]$weights)
+		}
+	# Check if directed edges have a complementary directed edge with the same weight in the opposite direction.
+		m.direct <- which(abs(m-t(m))!=0,arr.ind = T)
+		ifelse(length(m.direct)==0, TRUE, FALSE)
+}
--- hw4/is_valid.R
+++ hw4/is_valid.R
@@ -1,50 +1,26 @@
-#### 
-# Setup
-####
-
-###### Graph for Testing
-	# # Test Graph
-	# graph2 = list(A = list(edges   = c(2L),
-	#                        weights = c(14)),
-	#               B = list(edges   = c(3L,4L),
-	#                        weights = c(23,13)),
-	#               D = list(edges   = c(1L),
-	#                        weights = c(5) ),
-	#               F = list(edges   = c(1L,5L),
-	#                        weights = c(43,33)),
-	#               N = list(edges   = c(1L,2L,4L),
-	#                        weights = c(33,22,11)))
-
-	# g <- graph2 
-
 # Input - g, a graph object.
 # Output - true if valid, false if not.
-# Description - Validate the graph object to ensure that it meets all requirements -      
+# Description - Validate the graph object to ensure that it meets all requirements: 
 
-# Check that object is a list of lists.
-	test_that("is_valid", {
-		expect_true(is.list(g))
-		lapply(g, function(x){
-			expect_true(is.list(x))
-		})
-	})
-
-# Check if there are names for the primary list that they are all unique.
-	expect_equal(length(g), length(names(g)))
+is_valid = function(g) 
+{
+    # Check that object is a list of lists.
+   all( c(class(g), sapply(g, class)) == "list" )
+    # Check if there are names for the primary list that they are all unique.
+	length(g)==length(names(g))
 	expect_error(expect_match(t, "TRUE"))
-
-# Check that each secondary list contains only edges and weights vectors that are of the appropriate type.
+	# Check that each secondary list contains only edges and weights vectors that are of the appropriate type.
 	lapply(g, function(x){
 	  expect_identical(names(x), c("edges", "weights"))
 	})
-
-# Check that there are not any edges to non-existent vertices.
+	# Check that there are not any edges to non-existent vertices.
 	lapply(g, function(x){
 	  expect_equal(length(x$edges), length(x$weights))
 	})
-
-# Check that all weights are not less than or equal to 0.
+	# Check that all weights are not less than or equal to 0.
 	expect_false(c("FALSE") %in% as.character(
 		lapply(g, function(x){
 			x$weights > 0
 	})))
+	TRUE
+}
