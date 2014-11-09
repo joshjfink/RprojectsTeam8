f71c82cb3f272c986047938ae180c6bc401dde1c
YanyuLiu <yl304@stat.duke.edu>
2014-11-09 16:48:10 -0500

    fix a small problem

--- hw4/is_undirected.R
+++ hw4/is_undirected.R
@@ -8,7 +8,7 @@ is_undirected = function(g)
 		n = length(g)
 		m = matrix(0, ncol=n, nrow=n)
 		for(i in 1:n){
-			m[i, g[[i]]$edges] = ifelse(FALSE, g[[i]]$weights/sum(g[[i]]$weights), g[[i]]$weights)
+			m[i, g[[i]]$edges] = ifelse(array(0,length(g[[i]]$edges)), g[[i]]$weights/sum(g[[i]]$weights), g[[i]]$weights)
 		}
 	# Check if directed edges have a complementary directed edge with the same weight in the opposite direction.
 		m.direct <- which(abs(m-t(m))!=0,arr.ind = T)
