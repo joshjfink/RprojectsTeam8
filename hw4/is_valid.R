e5b8e9ef82bd1827fe3000d68405a22bde6679a0
joshua-james-f <joshua.james.f@gmail.com>
2014-11-10 00:05:06 -0500

    Fixed previously overlooked testing bug

--- hw4/is_valid.R
+++ hw4/is_valid.R
@@ -5,17 +5,17 @@
 is_valid = function(g) 
 {
 # Check that object is a list of lists.
-all(c(class(g), sapply(g, class)) == "list")
-length(g)==length(names(g))
+	all(c(class(g), sapply(g, class)) == "list")
+	length(g)==length(names(g))
 	# Check that there are not any edges to non-existent vertices.
-	lapply(g, function(x){
-	  expect_equal(length(x$edges), length(x$weights))
-	})
-	expect_error(expect_match(t, "TRUE"))
 	# Check that each secondary list contains only edges and weights vectors that are of the appropriate type.
-	for(i in 1:length(g)){
-	n <- names(g[[i]])== c("edges", "weights") | names(g[[i]])== c("weights", "edges")}
-	expect_match(n, "TRUE")
-	expect_error(expect_match(n, "FALSE"))
-	TRUE
+	e <- NULL
+	e <- sapply(g, function(x){
+	  all(max(x$edges) <= length(x))
+	})
+	ifelse(length(g[[1]])< 2,  FALSE,
+		ifelse(all(length(bad_g1[[1:length(bad_g1)]]$edges)!= length(bad_g1[[1:length(bad_g1)]]$weights)), FALSE,
+		ifelse(names(g[[1]])== c("edges", "weights"), all(e), 
+			ifelse(names(g[[1]])== c("weights", "edges"), all(e), FALSE))))
+	
 }
