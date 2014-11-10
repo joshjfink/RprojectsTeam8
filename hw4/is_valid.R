7deb8c1d7ed64c40537f7d72568af2aa64c025a1
joshua-james-f <joshua.james.f@gmail.com>
2014-11-10 01:38:30 -0500

    Fixed persistent bugs, the only test that this version does not pass is g4 in "valid graphs," which does not contain any data

--- hw4/is_valid.R
+++ hw4/is_valid.R
@@ -7,15 +7,25 @@ is_valid = function(g)
 # Check that object is a list of lists.
 	all(c(class(g), sapply(g, class)) == "list")
 	length(g)==length(names(g))
+	f <- sapply(g, function(x){
+	  ifelse(is.na(x$edges), FALSE,
+	  	ifelse(is.na(x$weights), FALSE,
+	  	ifelse(length(x$edges)!= length(x$weights), FALSE,
+	  		ifelse(x$weights < 1, FALSE,
+	  		ifelse(class(x$edges)=="integer", TRUE, FALSE)))))
+	})
 	# Check that there are not any edges to non-existent vertices.
 	# Check that each secondary list contains only edges and weights vectors that are of the appropriate type.
+		h <- sapply(g, function(x){
+		ifelse(duplicated(x$edges), FALSE, TRUE)
+	})
 	e <- NULL
+	l <- length(g)
 	e <- sapply(g, function(x){
-	  all(max(x$edges) <= length(x))
+	  all(max(x$edges) <= l)
 	})
 	ifelse(length(g[[1]])< 2,  FALSE,
-		ifelse(all(length(bad_g1[[1:length(bad_g1)]]$edges)!= length(bad_g1[[1:length(bad_g1)]]$weights)), FALSE,
-		ifelse(names(g[[1]])== c("edges", "weights"), all(e), 
-			ifelse(names(g[[1]])== c("weights", "edges"), all(e), FALSE))))
-	
+			ifelse(all(length(unique(names(g))) != length(names(g))), FALSE,
+		ifelse(names(g[[1]])== c("edges", "weights"), all(e, f, h), 
+			ifelse(names(g[[1]])== c("weights", "edges"), all(e, f, h), FALSE))))
 }
