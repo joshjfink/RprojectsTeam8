a6f764e2ae6e91e7950ee24d60395719ffb8ae69
Zheng Ding <jtappler@gmail.com>
2014-11-10 00:27:24 -0500

    String match done, got matrix with 3 cols or 2 cols (if weight do not exists)

--- hw4/read_graph.R
+++ hw4/read_graph.R
@@ -1,6 +1,7 @@
 read_graph = function(file){
-  library(stringr)
-  graph = as.matrix(read.table(file, fill = TRUE),ncol = 4)
+  graph = as.matrix(read.table(file, fill = TRUE), ncol = 4)
+  graph = subset(graph, select = -c(V2) ) # drop "->"
+  graph = sub(".*?weight=(.*?)].*", "\\1", graph)
   
   # some string matching here
   
