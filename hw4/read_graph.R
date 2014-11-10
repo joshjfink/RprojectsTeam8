aa01ea7d1714064c470f089ebe7718f0348786a0
Zheng Ding <jtappler@gmail.com>
2014-11-10 01:04:19 -0500

    create list for each

--- hw4/read_graph.R
+++ hw4/read_graph.R
@@ -3,8 +3,13 @@ read_graph = function(file){
   graph = subset(graph, select = -c(V2) ) # drop "->"
   graph = sub(".*?weight=(.*?)].*", "\\1", graph)
   graph = sub(";", "\\1", graph)
-  
-  
+  for (i in dim(graph2)[1]){
+    if (is.null(graph[i,])[3]){
+      g[[i]] = list(edges = as.character(graph[i,][2]))
+    } else{
+      g[[i]] = list(edges = as.character(graph[i,][2]), weights = as.numeric(graph[i,])[3])
+    }
+  }
   
   return(g)
 }
