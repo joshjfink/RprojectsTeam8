e282e36e9efe048a31908aea32192fc0f896e992
Zheng Ding <jtappler@gmail.com>
2014-11-09 19:29:36 -0500

    is_connected fucntion

---
+++ hw4/is_connected.R
@@ -0,0 +1,32 @@
+is_connected = function(g, v1, v2){
+  if(is.list(g)){
+    match=c(names(g), seq(1, length(g), by=1))
+    if(v1 %in% match & v2 %in% match){
+      if(length(c(g[[v1]]$edges)) == 0){
+        return(FALSE)
+      }
+      else{
+        if(is.character(v1)){
+          v1 = match(v1, names(g))
+        }
+        name = NULL
+        path = c(g[[v1]]$edges)
+        
+        for (i in 1:length(g)){
+          name = c(name, names(g)[path[i]])
+          path = c(path, g[[path[i]]]$edges)
+        }
+        if(v2 %in% path | v2 %in% name){
+          return(TRUE)
+        }
+      }
+      return(FALSE)
+    }
+    else{
+      return("error")
+    }
+  }
+  else{
+    return("error")
+  }
+}
