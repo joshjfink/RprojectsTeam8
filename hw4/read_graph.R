44c789adf86da49b2440eb5a0b11ba0ba34941ef
Zheng Ding <jtappler@gmail.com>
2014-11-10 00:27:53 -0500

    string match conflict fix

--- hw4/read_graph.R
+++ hw4/read_graph.R
@@ -2,11 +2,7 @@ read_graph = function(file){
   graph = as.matrix(read.table(file, fill = TRUE), ncol = 4)
   graph = subset(graph, select = -c(V2) ) # drop "->"
   graph = sub(".*?weight=(.*?)].*", "\\1", graph)
-  
-  # some string matching here
-  
-  
-  
+  graph = sub(";", "\\1", graph)
   
   
   
